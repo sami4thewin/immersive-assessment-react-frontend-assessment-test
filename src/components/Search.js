ebd3ff033609eba2bba65169f4e53d14b43dc700
sami4thewin <samiomarmohammed@gmail.com>
2017-03-29 06:07:00 -0400

    Done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,20 @@
-import React from 'react'
 
-const Search = ({searchTerm}) => {
-  return (
-    <div>
-      <input type="text" value={searchTerm.searchTerm} placeholder={"Search your most recent transactions"} />
-    </div>
-  )
+
+import React, { Component } from 'react'
+
+class Search extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.props.onChange} value={this.props.searchTerm} placeholder={"Search your most recent transactions"} />
+      </div>
+    )
+  }
+
 }
 
 export default Search
